c3b5d00db33a5e95dde6bb430b6d569a8a0b2743
Iryna <Iryna@Irynas-MBP-2.fritz.box>
2021-08-11 10:08:03 +0200

    Added first integration test

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
-
+require('./services/cache');
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI, {dbName: 'fi'});
 
 const app = express();
 
